7545a189f56b0231cedc5cc75427a26f945f7442
Geovani Miccoli <geovani.miccoli@gmail.com>
2025-02-17 09:52:22 -0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "FortiGate_Public_IP" {
-  value = ibm_is_floating_ip.publicip.address
-}
+# output "FortiGate_Public_IP" {
+#  value = ibm_is_floating_ip.publicip.address
+# }
 
 output "Custom_Image_Name" {
   description = "Your local FortiGate Custom Image reference"
